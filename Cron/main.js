1285a554ee3b0ed2e674c0b8e865469c474f78f6
Shakir <shakirrahman81@gmail.com>
2025-06-21 17:32:11 -0400

    Adds cron job

--- Cron/main.js
+++ Cron/main.js
@@ -1,27 +1,26 @@
 require("dotenv").config();
-const WebSocket = require("ws");
-const fs = require("fs");
 const axios = require("axios");
+const Redis = require("ioredis");
 
-const oAuth = process.env.OAUTH;
+const client = new Redis(`${process.env.UPSTASH_REDIS_URL}`);
 const CLIENT_ID = process.env.CLIENT_ID;
 const ACCESS_TOKEN = process.env.ACCESS_TOKEN;
 const BACKEND = process.env.BACKEND;
-const nick = "Shakbot";
-
-const socket = new WebSocket("wss://irc-ws.chat.twitch.tv:443");
+BATCH_SIZE = 100;
 
 const getLiveChannels = async (channels) => {
   try {
     const userLogins = channels.map((u) => `user_login=${u}`).join("&");
     const url = `https://api.twitch.tv/helix/streams?${userLogins}`;
+    console.log(url);
     const res = await axios.get(url, {
       headers: {
         "Client-ID": CLIENT_ID,
         Authorization: `Bearer ${ACCESS_TOKEN}`,
       },
     });
-    return res.data.data;
+    console.log(res.data.data);
+    return res.data.data.map((channel) => channel.user_login);
   } catch (err) {
     console.error("Live check error:", err.response?.data || err.message);
     return [];
@@ -29,28 +28,43 @@ const getLiveChannels = async (channels) => {
 };
 
 const getUserInfo = async () => {
-  const res = await fetch(`${BACKEND}/api/users/ids`);
+  const res = await fetch(`${BACKEND}/users/twitch-names`);
+  console.log("Got names!");
   return res.json();
 };
 
-exports.handler = async () => {
-  const data = await getUserInfo();
-  const channels = data.map((channel) => channel.twitch_name);
+const runJob = async () => {
+  console.log("Starting job!");
+  const channels = await getUserInfo();
   const liveChannels = [];
+  console.log(`Channels ${channels}`);
 
   for (let i = 0; i < channels.length; i += BATCH_SIZE) {
     const batch = channels.slice(i, i + BATCH_SIZE);
+    console.log(`Batch channels ${batch}`);
     const live = await getLiveChannels(batch);
-    liveChannels.push(live);
+    liveChannels.push(...live);
   }
 
-  // MAKE BATCH REQUEST TO SENDOU.INK API
+  console.log("Finished batching!");
 
-  // STORE RESULT IN CACHE
+  await client.set("live", JSON.stringify(liveChannels));
+  console.log("Saved live channels to Redis:", liveChannels);
+  await client.quit();
+  process.exit(0);
+};
 
-  console.log(result); // or save to DB, email, alert, etc.
+if (require.main === module) {
+  runJob().catch((err) => {
+    console.error("Error running cron job:", err);
+    process.exit(1);
+  });
+}
+
+exports.handler = async () => {
+  await runJob();
   return {
     statusCode: 200,
-    body: JSON.stringify(result),
+    body: JSON.stringify({ message: "Job ran successfully" }),
   };
 };
