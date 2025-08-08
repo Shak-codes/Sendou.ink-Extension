2ae54c85320d2813dbde2bf070ff8e477a986891
Shakir <shakirrahman81@gmail.com>
2025-08-07 20:03:22 -0400

    Styling

--- Frontend/src/config/screens/LinkProfile/LinkProfile.jsx
+++ Frontend/src/config/screens/LinkProfile/LinkProfile.jsx
@@ -12,38 +12,40 @@ const LinkProfile = ({
   error,
 }) => {
   return (
-    <>
-      <h2 className={styles.text}>Welcome!</h2>
-      <p className={styles.text}>
-        To set up this extension, please provide the 'User ID' of the Discord
-        account linked to your Sendou.ink account.
-      </p>
-      <Input
-        id="userid"
-        value={discordId}
-        onChange={onChange}
-        label="User ID"
-        help={
-          <>
-            Can't find your User ID? Read this{" "}
-            <a className={styles.link} href={URLS.USER_ID} target="_blank">
-              article
-            </a>
-          </>
-        }
-        error={error}
-      />
-      <div className={styles.bottomContainer}>
-        <Button
-          animated
-          text="Link Account"
-          onClick={initiateLink}
-          onError={onError}
-          onSuccess={onSuccess}
-          disabled={!discordId}
+    <section className={styles.container}>
+      <header>
+        <h2 className={styles.text}>Welcome!</h2>
+        <h3 className={styles.text}>
+          To set up this extension, please provide the 'User ID' of the Discord
+          account linked to your Sendou.ink account.
+        </h3>
+      </header>
+      <main>
+        <Input
+          id="userid"
+          value={discordId}
+          onChange={onChange}
+          label="User ID"
+          help={
+            <>
+              Can't find your User ID? Read this{" "}
+              <a className={styles.link} href={URLS.USER_ID} target="_blank">
+                article
+              </a>
+            </>
+          }
+          error={error}
         />
-      </div>
-    </>
+      </main>
+      <Button
+        animated
+        text="Link Account"
+        onClick={initiateLink}
+        onError={onError}
+        onSuccess={onSuccess}
+        disabled={!discordId}
+      />
+    </section>
   );
 };
 
