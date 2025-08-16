68f60352d852e5cd1bc1d1e0d51fc2d385455a7f
Shakir <shakirrahman81@gmail.com>
2025-08-15 20:14:57 -0400

    Updates profile

--- Frontend/src/components/Profile/Profile.tsx
+++ Frontend/src/components/Profile/Profile.tsx
@@ -1,6 +1,7 @@
 import styles from "./styles.module.scss";
 
 const Profile = ({ userData }) => {
+  console.log("User Data", userData);
   return (
     <main className={styles.profileContainer}>
       <a href={userData.sendou_url} target="_blank" rel="noopener noreferrer">
@@ -12,9 +13,28 @@ const Profile = ({ userData }) => {
           height="100"
         />
       </a>
-      <div className={styles.userInfo}>
-        <h2>{userData.sendou_name}</h2>
-      </div>
+      <section className={styles.userInfo}>
+        <h2 className={styles.userName}>{userData.sendou_name}</h2>
+        {!!userData.team && !!userData.team_url && (
+          <section className={styles.teamContainer}>
+            <img
+              className={styles.teamImage}
+              src={userData.team_url}
+              alt="Team Picture"
+              width="40"
+              height="40"
+            />
+            <section className={styles.teamDetails}>
+              <h4 className={styles.teamName}>{userData.team}</h4>
+              <h5 className={styles.teamRole}>{userData.team_role}</h5>
+            </section>
+          </section>
+        )}
+
+        {!!userData.sendouq_rank && (
+          <h3 className={styles.sqRank}>{userData.sendouq_rank}</h3>
+        )}
+      </section>
     </main>
   );
 };
